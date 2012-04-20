808de836bb242f7768929b247a6c93543592264a
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-04-21 08:44:34 +0900

    Initial
    [d]
    1. Set the content view
    
    [n]
    1. Add a touch listener to the button

---
+++ src/sample/touch/PianoTouchSample3Activity.java
@@ -0,0 +1,18 @@
+package sample.touch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PianoTouchSample3Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pianotouchsample3);
+        
+        // define a button
+//        savedInstanceState object = (savedInstanceState) findViewById(arguement);
+		
+        
+    }
+}
